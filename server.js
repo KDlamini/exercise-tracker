4303d6066757404de99bd2e7ee9889ee93c8a802
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-21 13:23:56 +0200

    added post request block of code

--- server.js
+++ server.js
@@ -23,15 +23,73 @@ const User = mongoose.model('User', new Schema({
 }));
 
 app.post('/api/exercise/new-user', (req, res) => {
-  console.log(req.body);
+  let username =  req.body.username;
 
-  // let newUser = new User({
-  // });
-  res.json(req.body);
+  // User.deleteMany({username: username}, (error, result) => {
+  //   if(error) {
+  //     console.log(error);
+  //   } else {
+  //     res.json(result);
+  //   }
+  // })
+
+  User.findOne({username: username}, (error, result) => {
+    if(error) {
+      console.log(error);
+    }
+    else if (result == null) {
+      let newUser = new User({
+        username: username,
+        date: "",
+        duration: 0,
+        description: ""
+      });
+    
+      newUser.save((error, doc) => {
+        if(error) {
+          console.log(error);
+        } else {
+          console.log("Successfuly inserted document!")
+          res.json({
+            username: doc.username,
+            _id: doc._id
+          })
+        }
+      });
+    } else {
+      res.json(result);
+      // res.send('Username already taken');
+    }
+  });
+
 });
 
 app.post('/api/exercise/add', (req, res) => {
-  res.json({});
+  let inputData = req.body;
+  let inputDate = new Date(inputData.date)
+
+  if(inputData.duration < 1) {
+    res.send('duration too short');
+  }
+  
+  User.findById(inputData.userId)
+      .select('-__v')
+      .exec((err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        result.date = inputDate;
+        result.duration = Number(inputData.duration);
+        result.description = inputData.description;
+
+        result.save((error, doc) => {
+          if(error) {
+            console.log(error);
+          } else {
+            res.json(doc);
+          }
+        });
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
